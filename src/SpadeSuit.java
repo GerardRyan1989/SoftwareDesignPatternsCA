e2e4ba6a56cdd8f12f0754c51a38650323429c7d
Gerad Ryan <Gerardryan1989@gmail.com>
2019-02-01 13:40:06 +0000

    Team City test commit 2

--- src/SpadeSuit.java
+++ src/SpadeSuit.java
@@ -51,7 +51,7 @@ public class SpadeSuit extends CardSuit {
                 case 12:
                     card = new SpadePlayingCard(10,"King", new ImageIcon("cards/KingSpade.jpg"));
                     break;
-                    //test for team City
+                    //test for team City 2020
             }
 
             spades.add(card);
