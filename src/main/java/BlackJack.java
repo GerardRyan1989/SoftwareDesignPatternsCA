1a4b4e6974208588a34970a5e9987d88961dd84d
Gerad Ryan <Gerardryan1989@gmail.com>
2019-03-06 12:13:46 +0000

    Heroku Test

--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -2,6 +2,8 @@ public class BlackJack{
 	public static void main(String [] args){
 	MainMenuGUI mainMenu = new MainMenuGUI();//instansiating a new MainMenu GUI
 	}//end of main
+
+	//heroku Test
 }//end of class
 
 
